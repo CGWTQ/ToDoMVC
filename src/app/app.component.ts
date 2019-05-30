28fc5df94dcd48ebfa9a5818851d6ec420fa626b
“CGWTQe <375091805@qq.com>
2019-05-31 00:38:10 +0800

    完成列表渲染   2019-05-31

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+const todos =[
+  {
+    id:1,
+    title:"吃饭",
+    done:true
+  },
+  {
+    id:2,
+    title:"睡觉",
+    done:true
+  }
+]
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ToDoMVC-Angular';
+  //记录信息
+  public todos:{
+    id:number,
+    title:string,
+    done:boolean
+  }[] = todos
 }
