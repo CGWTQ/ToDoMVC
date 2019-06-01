ccc84643b75dbc2704774159fc619c8b5333dc18
“CGWTQe <375091805@qq.com>
2019-06-01 16:59:56 +0800

    列表  20190601

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 const todos =[
   {
     id:1,
@@ -17,7 +18,8 @@ const todos =[
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
   //记录信息
@@ -26,4 +28,45 @@ export class AppComponent {
     title:string,
     done:boolean
   }[] = todos
+
+  public currentEditing:{
+    id:number,
+    title:string,
+    done:boolean
+  }[] = null
+
+  addtodo(e){
+    const titleText = e.target.value;
+    if( !(titleText.length) ){
+      return
+    }
+    const last = this.todos[this.todos.length - 1]
+    this.todos.push({
+      id:last ? last.id + 1 : 1,
+      title:titleText,
+      done:false
+    })
+
+    //添加完成清除文本框
+    e.target.value = ""
+    
+  }
+  //所有任务图标
+  get toggleAll(){
+    return this.todos.every( t => t.done)
+  }
+  set toggleAll(val){
+    this.todos.forEach(t => t.done = val)
+  }
+
+  //删除任务
+  removetodo(index:number){
+    this.todos.splice(index,1);
+    
+  }
+  test(){
+    console.log(11);
+    
+  }
+  
 }
