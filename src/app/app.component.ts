7fc0194732c5317a7beaca6cf22670d118ba8df4
“CGWTQe <375091805@qq.com>
2019-06-01 23:12:00 +0800

    ToDoMVC over  - 20190601

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 
 
-const todos =[
-  {
-    id:1,
-    title:"吃饭",
-    done:true
-  },
-  {
-    id:2,
-    title:"睡觉",
-    done:true
-  }
-]
+// const todos =[
+//   {
+//     id:1,
+//     title:"吃饭",
+//     done:true
+//   },
+//   {
+//     id:2,
+//     title:"睡觉",
+//     done:false
+//   }
+// ]
 
 
 @Component({
@@ -27,13 +27,35 @@ export class AppComponent {
     id:number,
     title:string,
     done:boolean
-  }[] = todos
+  }[] = JSON.parse(window.localStorage.getItem("todos") || "[]");
 
   public currentEditing:{
     id:number,
     title:string,
     done:boolean
   }[] = null
+
+
+  public visibility:string = "all"
+
+  //生命周期函数（初始化时执行一次）
+  ngOnInit(): void {
+    this.hashChange();
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    //注意  bind  绑定 this 指向
+    window.onhashchange  = this.hashChange.bind(this);
+    
+  }
+
+  //每当数据改变便触发
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    //Add 'implements DoCheck' to the class.
+    window.localStorage.setItem("todos",JSON.stringify(this.todos));
+  }
+
 
   addtodo(e){
     const titleText = e.target.value;
@@ -64,9 +86,60 @@ export class AppComponent {
     this.todos.splice(index,1);
     
   }
+  //保存编辑
+  saveEdit(todo,e){
+    todo.title = e.target.value;
+    this.currentEditing = null;
+    
+  }
+  //取消编辑
+  cansal(e){
+    const { keyCode,target } = e;
+    if(keyCode === 27){
+      target.value = this.currentEditing.title;
+      this.currentEditing = null;
+    }  
+  }
+
+  //未完成的任务数量
+  get remaningCount(){
+    return this.todos.filter(t => !t.done).length
+  }
+  //清除所有已完成任务
+  clearAllDone(){
+    this.todos = this.todos.filter(t => !t.done);
+  }
+
+  get filterTodos(){
+    if(this.visibility === "all"){
+      return this.todos;
+    }else if(this.visibility === "active"){
+      return this.todos.filter( t => !t.done);
+    }else if(this.visibility === "Completed"){
+      return this.todos.filter(t => t.done);
+    }
+  }
+
+  //路由改变
+  hashChange(){
+    const hash = window.location.hash.substr(1)
+      switch(hash){
+        case "/":
+          this.visibility = "all";
+          break;
+        case "/active":
+          this.visibility = "active";
+          break;
+        case "/completed":
+          this.visibility = "Completed";
+          break;
+      }
+  }
+
   test(){
     console.log(11);
     
   }
   
 }
+
